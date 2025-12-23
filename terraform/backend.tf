423520098dd3dfc9018c3fbd0889d543c54d8586
AjinkyaP-09 <pameajinkya09@gmail.com>
2025-12-23 11:25:02 +0000

    Move files to terraform folder and change bucket name.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # REPLACE with the actual bucket name you created in Step 1
-    bucket         = "my-terraform-state-bucket-unique-12345"
+    bucket         = "my-terraform-state-bucket-unique-12345-23122025"
     
     # This is the folder/file path inside the bucket
     key            = "eks-cluster/terraform.tfstate"
